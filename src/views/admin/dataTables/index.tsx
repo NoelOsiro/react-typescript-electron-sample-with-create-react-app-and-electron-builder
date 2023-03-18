2037b4b0465f675f831abd404c337be1a792b287
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2023-03-18 11:33:21 +0000

    Finished index.tsx

---
+++ src/views/admin/dataTables/index.tsx
@@ -0,0 +1,24 @@
+// Chakra imports
+import { Box, SimpleGrid } from '@chakra-ui/react';
+import DevelopmentTable from './components/DevelopmentTable';
+import CheckTable from './components/CheckTable';
+import ColumnsTable from './components/ColumnsTable';
+import ComplexTable from './components/ComplexTable'; 
+import tableDataDevelopment from './variables/tableDataDevelopment';
+import tableDataCheck from './variables/tableDataCheck';
+import tableDataColumns from './variables/tableDataColumns';
+import tableDataComplex from './variables/tableDataComplex';
+
+export default function Settings() {
+	// Chakra Color Mode
+	return (
+		<Box pt={{ base: '130px', md: '80px', xl: '80px' }}>
+			<SimpleGrid mb='20px' columns={{ sm: 1, md: 2 }} spacing={{ base: '20px', xl: '20px' }}>
+				<DevelopmentTable   tableData={tableDataDevelopment} />
+				<CheckTable tableData={tableDataCheck} />
+				<ColumnsTable  tableData={tableDataColumns} />
+				<ComplexTable tableData={tableDataComplex} />
+			</SimpleGrid>
+		</Box>
+	);
+}
