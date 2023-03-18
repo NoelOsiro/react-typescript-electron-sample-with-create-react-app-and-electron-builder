9da0f7749ea080d96b4d7c90435403545a02c5cd
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2023-03-18 21:57:24 +0000

    More fixing

--- src/components/navbar/NavbarLinksAdmin.tsx
+++ src/components/navbar/NavbarLinksAdmin.tsx
@@ -15,9 +15,9 @@ import {
 	useColorMode
 } from '@chakra-ui/react';
 // Custom Components
-import { ItemContent } from 'components/menu/ItemContent';
-import { SearchBar } from 'components/navbar/searchBar/SearchBar';
-import { SidebarResponsive } from 'components/sidebar/Sidebar';
+import { ItemContent } from '../menu/ItemContent';
+import { SearchBar } from '../navbar/searchBar/SearchBar';
+import { SidebarResponsive } from '../sidebar/Sidebar';
 import PropTypes from 'prop-types';
 import React from 'react';
 // Assets
@@ -25,7 +25,7 @@ import navImage from 'assets/img/layout/Navbar.png';
 import { MdNotificationsNone, MdInfoOutline } from 'react-icons/md';
 import { IoMdMoon, IoMdSunny } from 'react-icons/io';
 import { FaEthereum } from 'react-icons/fa';
-import routes from 'routes';
+import routes from '../../routes';
 export default function HeaderLinks(props: { secondary: boolean }) {
 	const { secondary } = props;
 	const { colorMode, toggleColorMode } = useColorMode();
